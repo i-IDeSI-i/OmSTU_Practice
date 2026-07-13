0fa6424bfe2924854ca921aee5a54ee62a311211
i-IDeSI-i <esv2022@mail.ru>
2026-07-14 03:19:50 +0600

    feat:task02

---
+++ task02/Student.cs
@@ -0,0 +1,8 @@
+namespace task02;
+
+public class Student
+{
+    public string Name { get; set; } = string.Empty;
+    public string Faculty { get; set; } = string.Empty;
+    public List<int> Grades { get; set; } = new();
+}
