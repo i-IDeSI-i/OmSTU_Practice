9324eccc6bac69916984aece5bd5c3f95ce6016e
i-IDeSI-i <esv2022@mail.ru>
2026-07-15 01:50:29 +0600

    feat: task08

---
+++ CommandRunner/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using CommandLib;
+
+string dllPath = Path.Combine(AppContext.BaseDirectory, "FileSystemCommands.dll");
+
+if (!File.Exists(dllPath))
+{
+    Console.WriteLine($"Не найдена библиотека команд: {dllPath}");
+    return;
+}
+
+Assembly assembly = Assembly.LoadFrom(dllPath);
+
+string demoDir = Path.GetTempPath();
+
+Type sizeType = assembly.GetType("FileSystemCommands.DirectorySizeCommand")!;
+ICommand sizeCommand = (ICommand)Activator.CreateInstance(sizeType, demoDir)!;
+sizeCommand.Execute();
+
+Type findType = assembly.GetType("FileSystemCommands.FindFilesCommand")!;
+ICommand findCommand = (ICommand)Activator.CreateInstance(findType, demoDir, "*.txt")!;
+findCommand.Execute();
