0198d901e4c1ed0d9c051e0b7bef781c6f41624f
i-IDeSI-i <esv2022@mail.ru>
2026-07-18 01:03:47 +0600

    feat: task19  add round-robin visualization plot

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,26 +1,49 @@
-﻿using task17;
+using ScottPlot;
+using task17;
 using task18;
 using task19;
 
-Console.WriteLine("Демонстрация длительных операций (task19) \n");
+Console.WriteLine("=== Демонстрация длительных операций (task19) ===\n");
+
+var xs = new List<double>();
+var ys = new List<double>();
+int globalStep = 0;
+var dataLock = new object();
+
+void Record(int id)
+{
+    lock (dataLock)
+    {
+        xs.Add(id);
+        ys.Add(globalStep);
+        globalStep++;
+    }
+}
 
 var server = new SchedulerThread();
 server.Start();
 
-for (int id = 1; id <= 5; id++)
+for (int id = 0; id < 5; id++)
 {
-    server.EnqueueCommand(new TestCommand(id, maxCalls: 3));
+    int localId = id;
+    server.EnqueueCommand(new TestCommand(localId, maxCalls: 4, onExecute: () => Record(localId)));
 }
 
 Thread.Sleep(500);
 
-Console.WriteLine("\nВсе команды выполнены по 3 раза. Останавливаем поток (HardStop)");
-
+Console.WriteLine("\n=== Все команды выполнены. Останавливаем поток ===");
 server.Stop();
-server.EnqueueCommand(new SimpleStopSignal()); 
+server.EnqueueCommand(new SimpleStopSignal());
 server.Join();
-
 Console.WriteLine($"Поток остановлен. Живой: {server.IsAlive}");
+
+var plt = new Plot();
+plt.Add.ScatterPoints(xs.ToArray(), ys.ToArray());
+plt.XLabel("Ид задачи");
+plt.YLabel("Сквозной шаг");
+plt.Title("Сквозной шаг от ид задачи");
+plt.SavePng("task19.png", 700, 500);
+Console.WriteLine("График сохранён: task19.png");
 
 public class SimpleStopSignal : ICommand
 {
--- task19/TestCommand.cs
+++ task19/TestCommand.cs
@@ -7,16 +7,17 @@ public class TestCommand : ILongRunningCommand
 {
     private readonly int _id;
     private readonly int _maxCalls;
+    private readonly Action? _onExecute;
     private int _counter = 0;
 
-    public TestCommand(int id, int maxCalls = 3)
+    public TestCommand(int id, int maxCalls = 3, Action? onExecute = null)
     {
         _id = id;
         _maxCalls = maxCalls;
+        _onExecute = onExecute;
     }
 
     public bool IsCompleted => _counter >= _maxCalls;
-
     public int Counter => _counter;
 
     public void Execute()
@@ -26,5 +27,6 @@ public class TestCommand : ILongRunningCommand
 
         _counter++;
         Console.WriteLine($"Поток {_id} вызов {_counter}");
+        _onExecute?.Invoke();
     }
 }
